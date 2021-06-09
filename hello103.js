3860e0ba77853ef3945c88aeee1703b56a0758f7
bumrei <bumrei@gmail.com>
2021-06-09 16:48:19 +0900

    수정2

--- hello103.js
+++ hello103.js
@@ -3,3 +3,4 @@ console.log("전 노트북도 샀어요..tmi");
 console.log("파이팅!");
 console.log("오 노트북 뭐 사셨어요? 저 멕 사고싶네요");
 console.log("맥은 너무 비싸서 저 그램 샀어요!!");
+console.log("오 그램!! 제 친구도 그거 사용하는데 가볍고 완전 좋더라구요ㅎㅎ");
