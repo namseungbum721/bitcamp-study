672db30cecbd7daa7a3eee848f05e0ead417e83d
namseungbum721 <oposa721@naver.com>
2021-06-09 15:35:26 +0900

    x

--- hello3.js
+++ hello3.js
@@ -1,6 +1,6 @@
 console.log("나는 멋진 사람이야"); 
 console.log("Hello, world!");
 console.log("beautiful world!"); 
-console.log("나는 긍정적인 사람이다 보니까 어렵지 않아"); 
-console.log("재미진 수업이야, 유레카!"); 
+console.log("나는 멋진 사람이야"); 
+
 
