a0b412f905c4183f7e5bc788433cae3d94b72891
namseungbum721 <oposa721@naver.com>
2021-07-05 18:12:20 +0900

    SQL

---
+++ Mtest/winter.sql
@@ -0,0 +1,11 @@
+drop table winter;
+
+create table winter (
+sabun number(4) not null primary key,
+name varchar(10) not null,
+buse varchar(15) not null
+);
+
+insert into winter(sabun, name, buse) values(1100, 'nam', 'ssss');
+insert into winter(sabun, name, buse) values(1200, 'seung', 'qqqq');
+insert into winter(sabun, name, buse) values(1300, 'bum', 'llll');
